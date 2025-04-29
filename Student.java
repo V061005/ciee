636b82bef40028046c699d3f1e8c53eaf361c0d2
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-29 14:48:48 +0530

    Save it in main

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public static void main(String[] args) {
+        String name="Vaishnavi";
+        int birthyear=2005;
+        int currentyear=2025;
+
+        int age= currentyear-birthyear;
+
+        System.err.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+}
